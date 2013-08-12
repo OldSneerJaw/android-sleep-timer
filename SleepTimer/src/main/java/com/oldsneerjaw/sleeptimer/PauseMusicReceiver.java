9673cfa3726941921f8b7972b2a5fe9c37cb3ba8
Joel Andrews <joelaandrews@gmail.com>
2013-08-11 20:41:45 -0700

    Implemented a service to handle requesting and holding onto audio focus indefinitely so that Rdio playback remains paused. Read more: https://github.com/rdio/api/issues/90

--- SleepTimer/src/main/java/com/oldsneerjaw/sleeptimer/PauseMusicReceiver.java
+++ SleepTimer/src/main/java/com/oldsneerjaw/sleeptimer/PauseMusicReceiver.java
@@ -8,8 +8,6 @@ package com.oldsneerjaw.sleeptimer;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.media.AudioManager;
-import android.util.Log;
 
 /**
  * Handles broadcast events intended to pause music playback indefinitely.
@@ -18,90 +16,23 @@ import android.util.Log;
  */
 public class PauseMusicReceiver extends BroadcastReceiver {
 
-    private static final String LOG_TAG = PauseMusicReceiver.class.getName();
-
     @Override
     public void onReceive(Context context, Intent intent) {
-
-        if (pauseMusicPlayback(context)) {
-            Log.i(LOG_TAG, "Successfully paused music playback");
-
-            notify(context);
-        } else {
-            Log.e(LOG_TAG, "Failed to pause music playback");
-        }
-
-        TimerManager.getInstance(context).cancelTimer();
+        pauseMusic(context, TimerManager.getInstance(context));
     }
 
     /**
      * Pauses all music playback on the device.
      *
-     * @param context The context
+     * @param context The context in which the receiver is running
      *
-     * @return {@code true} if playback was successfully paused; otherwise, {@code false}
+     * @param timerManager The pause music timer manager
      */
-    private boolean pauseMusicPlayback(Context context) {
+    void pauseMusic(Context context, TimerManager timerManager) {
+        // The service will be responsible for actually pausing playback and ensuring it remains paused until explicitly
+        // restarted
+        context.startService(new Intent(context, PauseMusicService.class));
 
-        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-
-        AudioFocusListener listener = new AudioFocusListener(audioManager);
-
-        // A well-behaved media player should relinquish audio focus (i.e. pause playback) when another app requests
-        // focus: http://developer.android.com/training/managing-audio/audio-focus.html#HandleFocusLoss
-        int audioFocusResult = audioManager.requestAudioFocus(listener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
-        if (audioFocusResult == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
-            Log.d(LOG_TAG, "Audio focus GAINED");
-
-            // Immediately release focus. If the previous owner is well behaved, it will remain paused indefinitely;
-            // if not, then holding onto audio focus will only briefly delay it from resuming playback until this app
-            // is automatically recycled by the OS
-            audioManager.abandonAudioFocus(listener);
-
-            return true;
-        } else {
-            Log.d(LOG_TAG, "Audio focus DENIED");
-
-            return false;
-        }
-    }
-
-    /**
-     * Posts a status bar notification that music playback has been paused.
-     *
-     * @param context The context
-     */
-    private void notify(Context context) {
-        PauseMusicNotifier notifier = new PauseMusicNotifier(context);
-        notifier.postNotification();
-    }
-
-
-    /**
-     * A listener for audio focus change events.
-     */
-    private class AudioFocusListener implements AudioManager.OnAudioFocusChangeListener {
-
-        private AudioManager audioManager;
-
-        public AudioFocusListener(AudioManager audioManager) {
-            this.audioManager = audioManager;
-        }
-
-        @Override
-        public void onAudioFocusChange(int focusChange) {
-
-            switch (focusChange) {
-                case AudioManager.AUDIOFOCUS_GAIN:
-                    Log.d(LOG_TAG, "Audio focus REGAINED");
-                    break;
-                case AudioManager.AUDIOFOCUS_LOSS:
-                    Log.d(LOG_TAG, "Audio focus LOST permanently");
-                    audioManager.abandonAudioFocus(this);
-                    break;
-                default:
-                    Log.d(LOG_TAG, "Audio focus changed: " + focusChange);
-            }
-        }
+        timerManager.cancelTimer();
     }
 }
